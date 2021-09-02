b9cf4d641586aea6585dd74d5d5e1542ddeaa2c9
andresvd1995 <andresvd95@hotmail.com>
2021-09-01 20:57:17 -0500

    javascript

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,10 +1,18 @@
 var slideIndex =1;
 showslides (slideIndex)
 
+function plusSlides(n) {
+    showslides(slideIndex += n);
+ }
+
+ function currentSlide(n){
+    showslides(slideIndex = n);
+
+ }
+
 function showslides(n) {
-    var i;
     var slides = document.getElementsByClassName("myslides");
-    var dot = document.getElementsByClassName("dot");
+    var dots = document.getElementsByClassName("dot");
 
     if (n > slides.length) {
         slideIndex = 1;
@@ -13,9 +21,23 @@ function showslides(n) {
     if (n < 1) {
         slideIndex = slides.length;
     }
+
     for (let i = 0; i < slides.length; i++) {
        slides[i].style.display = "none";
         
     }
+
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className.replace(" active", "");
+        
+    }
+
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active"
     
+    slideIndex++;
+
+    setTimeout(() => {
+        showslides(slideIndex);
+    }, 6000);
 }
